fe5710ea6856476dfc1eba1b3915f595baa6e023
Ifleex-Solution <ifleexinternal@gmail.com>
2026-01-11 16:54:43 +0530

    git ignore file changes

---
+++ application/config/database.php
@@ -0,0 +1,77 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$active_group = 'default';
+$query_builder = TRUE;
+$active_record = TRUE;//ci version 2.x
+// $db['default'] = array(
+//     'dsn'   => '',
+//     'hostname' => 'localhost',
+//     'username' => 'root',
+//     'password' => '',
+//     'database' => 'betalitelegacy',
+//     'dbdriver' => 'mysqli',
+//     'max_allowed_packet'=>'200M',
+//     'dbprefix' => '',
+//     'pconnect' => FALSE,
+//     'db_debug' => (ENVIRONMENT !== 'production'),
+//     'cache_on' => FALSE,
+//     'cachedir' => '',
+//     'char_set' => 'utf8',
+//     'dbcollat' => 'utf8_general_ci',
+//     'swap_pre' => '',
+//     'encrypt'  => FALSE,
+//     'compress' => FALSE,
+//     'autoinit' => TRUE,//ci version 2.x
+//     'stricton' => FALSE,
+//     'failover' => array(),
+//     'save_queries' => TRUE
+// );
+
+$db['default'] = array(
+    'dsn'   => '',
+    'hostname' => 'localhost',
+    'username' => 'fextenso_admin1',
+    'password' => '4IB49p74odV8',
+    'database' => 'fextenso_svmvddev',
+    'dbdriver' => 'mysqli',
+    'max_allowed_packet'=>'200M',
+    'dbprefix' => '',
+    'pconnect' => FALSE,
+    'db_debug' => (ENVIRONMENT !== 'production'),
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt'  => FALSE,
+    'compress' => FALSE,
+    'autoinit' => TRUE,//ci version 2.x
+    'stricton' => FALSE,
+    'failover' => array(),
+    'save_queries' => TRUE
+);
+//
+//$db['default'] = array(
+//    'dsn'   => '',
+//    'hostname' => 'localhost',
+//    'username' => 'selvakum_erp',
+//    'password' => '-1^aC*V9Xv.7',
+//    'database' => 'selvakum_erp',
+//    'dbdriver' => 'mysqli',
+//    'max_allowed_packet'=>'200M',
+//    'dbprefix' => '',
+//    'pconnect' => FALSE,
+//    'db_debug' => (ENVIRONMENT !== 'production'),
+//    'cache_on' => FALSE,
+//    'cachedir' => '',
+//    'char_set' => 'utf8',
+//    'dbcollat' => 'utf8_general_ci',
+//    'swap_pre' => '',
+//    'encrypt'  => FALSE,
+//    'compress' => FALSE,
+//    'autoinit' => TRUE,//ci version 2.x
+//    'stricton' => FALSE,
+//    'failover' => array(),
+//    'save_queries' => TRUE
+//);
